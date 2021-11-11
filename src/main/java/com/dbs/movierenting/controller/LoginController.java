73923d5bd8760f4cd47b7d0ebc136007def87b9d
Vaibhavx10 <34233078+Vaibhavx10@users.noreply.github.com>
2021-11-11 15:30:15 +0000

    boiller plate added

--- src/main/java/com/dbs/movierenting/controller/LoginController.java
+++ src/main/java/com/dbs/movierenting/controller/LoginController.java
@@ -1,17 +1,52 @@
 package com.dbs.movierenting.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
-	 
-	@RequestMapping(value="/", method = RequestMethod.GET)
-	public String loginhome(){
-		System.out.println("Hey base url called");
-		return "redirect:view/dashboard.jsp";
+
+	/*
+	 * @RequestMapping(value = "/", method = RequestMethod.GET) public String
+	 * baseLogin() { System.out.println("Hey base url called"); return
+	 * "redirect:view/login.jsp"; }
+	 */
+
+	@RequestMapping(value = "view/userValidation", method = RequestMethod.POST)
+	public String loginhome(ModelMap model, HttpServletRequest request) {
+
+		String uname = request.getParameter("uname");
+		String upass = request.getParameter("upass");
+		System.out.println("loginhome :: " + uname + " pass :: " + upass);
+		System.out.println("loginhome :: " + uname.hashCode());
+		System.out.println("loginhome :: " + "a".hashCode());
+		
+		
+		if (uname.hashCode() == "a".hashCode()) {
+			System.out.println("Hey base url called");
+			return "redirect:/view/dashboard.jsp";
+		} else {
+			return "redirect:/view/login.jsp";
+		}
 	}
-	
-	
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String temp() {
+		System.out.println("Hey base url called");
+		return "redirect:view/login.jsp";
+	}
+
+	public static void main(String[] args) {
+		
+		String a  = "a";
+		if (a.equals("a")) {
+			System.out.println("All Good !! ");
+		}
+
+	}
+
 }
--- src/main/webapp/view/login.jsp
+++ src/main/webapp/view/login.jsp
@@ -1,12 +1,29 @@
 <%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <html>
-<head><title>Login</title></head>
+<head>
+<title>Login Page</title>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Bootstrap CSS -->
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+	crossorigin="anonymous">
+<link type="text/css" href="../css/dashboardstyle.css" rel="stylesheet" />
+
+</head>
+
 <body>
-  <form:form action="userCheck" method="post">
-   Enter User Name:<input type="text" name="name"> <br/>
-   Enter Password :<input type="password" name="pwd"/><br/>
-   <input type="submit">
-   
-  </form:form>
+	<form:form action="userValidation" method="post">
+		<div class="container card card-body">
+			Enter User Name:<input type="text" name="uname"> <br /> 
+			Enter Password :<input type="password" name="upass" /> <br /> 
+			<input 	type="submit">
+		</div>
+	</form:form>
 </body>
+
 </html>
